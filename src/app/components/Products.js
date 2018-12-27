a0dc027d69d18af48204852c2a0b6f3b8312bc3c
Krishank Dwivedi <kdwivedi2@sapient.com>
2018-12-27 03:28:25 +0000

    add to cart half funcationality

--- src/app/components/Products.js
+++ src/app/components/Products.js
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 export class Products extends React.Component{
   constructor(props){
     super();
-    this.products = "";
+    this.cartHtml = "";
+    this.setCartData=[];
     this.state = {
-      "products": ""
+      "products": "",
+      "cartData":""
     }
   }
 
@@ -16,44 +18,51 @@ export class Products extends React.Component{
       {
         "name": "Item One",
         "link": "#",
-        "price":"$24.99",
+        "price":"24.99",
         "dec": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!",
         "image": "http://placehold.it/700x400",
         "raiting": 4,
-        "addToCart": false
+        "addToCart": false,
+        "btnText": "Add To Cart",
+        "quantity": 1
       },
       {
         "name": "Item Two",
         "link": "#",
-        "price":"$24.99",
+        "price":"24.99",
         "dec": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!",
         "image": "http://placehold.it/700x400",
         "raiting": 4,
-        "addToCart": false
+        "addToCart": false,
+        "btnText": "Add To Cart",
+        "quantity": 1
       },
       {
         "name": "Item Three",
         "link": "#",
-        "price":"$24.99",
+        "price":"24.99",
         "dec": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!",
         "image": "http://placehold.it/700x400",
         "raiting": 4,
-        "addToCart": false
+        "addToCart": false,
+        "btnText": "Add To Cart",
+        "quantity": 1
       },
       {
         "name": "Item Four",
         "link": "#",
-        "price":"$24.99",
+        "price":"24.99",
         "dec": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!",
         "image": "http://placehold.it/700x400",
         "raiting": 4,
-        "addToCart": false
+        "addToCart": false,
+        "btnText": "Add To Cart",
+        "quantity": 1
       }
-    ]
+    ];
   }
 
   showBtn(key){
-    console.log(this);
     this.state.products[key].addToCart=true;
     this.setState({
       "products": this.state.products
@@ -79,7 +88,7 @@ export class Products extends React.Component{
                 <h4 className="card-title">
                   <a href="#">{product.name}</a>
                 </h4>
-                <h5>{product.price}</h5>
+                <h5>${product.price}</h5>
                 <p className="card-text">{product.dec}</p>
               </div>
               <div className="card-footer" raiting={product.raiting}>
@@ -88,11 +97,48 @@ export class Products extends React.Component{
             </div>
             {(() => {
               if(product.addToCart) {
-                return <button className="btn btn-primary" >Add To Cart</button>
+                return <button className=" col-lg-12 col-md-6 btn btn-primary" onClick={this.updateCart.bind(this, index)} >{product.btnText}</button>
               }
             })()}
 
          </div>
+      )
+    });
+
+  }
+
+  updateCart(key){
+    if(this.state.products[key].btnText!=="Added"){
+      this.state.products[key].btnText="Added";
+      this.setCartData.push({
+        "name": this.state.products[key].name,
+        "price": this.state.products[key].price,
+        "quantity": this.state.products[key].quantity
+      })
+      this.state.cartData = this.setCartData
+      this.setState({
+        "products": this.state.products,
+        "cartData": this.state.cartData
+      });
+      this.genrateCart(this.state.cartData);
+    }
+  }
+
+
+  genrateCart(cartData){
+
+    this.cartHtml = cartData.map((item, i)=>{
+      let total = 0;
+      total= total+parseFloat(item.price, 10)
+      return (
+        <div className="list-group">
+          <a href="#" className="list-group-item">
+            Item: {item.name}
+            and
+            Price: {item.price}
+          </a>
+          <a href="#" className="list-group-item">Total: {total}</a>
+        </div>
       )
     });
 
@@ -104,12 +150,7 @@ export class Products extends React.Component{
             <div className="col-lg-3">
 
               <h1 className="my-4">Cart</h1>
-              <div className="list-group">
-                <a href="#" className="list-group-item">Category 1</a>
-                <a href="#" className="list-group-item">Category 2</a>
-                <a href="#" className="list-group-item">Category 3</a>
-              </div>
-
+              {this.cartHtml}
             </div>
 
             <div className="col-lg-9">
